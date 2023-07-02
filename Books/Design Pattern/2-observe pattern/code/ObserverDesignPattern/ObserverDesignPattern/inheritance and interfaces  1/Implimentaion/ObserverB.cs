7357df5c8143df6d68b642655d3ce6201d4b4e5e
m.delzendeh <m.delzendeh72@gmail.com>
2023-07-02 18:58:58 +0330

    observe code base

---
+++ Books/Design Pattern/2-observe pattern/code/ObserverDesignPattern/ObserverDesignPattern/inheritance and interfaces  1/Implimentaion/ObserverB.cs
@@ -0,0 +1,24 @@
+﻿using ObserverDesignPattern.Interfacse;
+
+namespace ObserverDesignPattern.Implimentaion
+{
+    /// <summary>
+    /// observer are follow subject and should know it
+    /// </summary>
+    public class ObserverB : IObserver
+    {
+        private Subject subject;
+
+        public ObserverB(Subject subject)
+        {
+            this.subject = subject;
+            subject.Attach(this);
+        }
+
+        public void Update()
+        {
+            Console.WriteLine("Observer B: Subject's state has changed to " + subject.State);
+        }
+    }
+
+}
